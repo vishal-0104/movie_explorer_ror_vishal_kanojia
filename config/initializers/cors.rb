6e297b3ae85cf5015b160bc2c6f983dfb1c75874
Vishal Kanojia <vishal.kanojia@magicsw.com>
2025-05-01 16:21:36 +0530

    [vishal]Mod: modified for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: false
-      expose: ['Authorization']
   end
 end
